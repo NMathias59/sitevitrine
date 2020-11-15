c8af1488ef56640303fc0e8279b00e59b10f7210
Nieuwjaer <mathias.nieuwjaer@gmail.com>
2020-11-15 21:35:46 +0100

    creation de la partie decription et mes projet

--- src/components/profil.jsx
+++ src/components/profil.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Profil = (props) => {
+const Profil = () => {
     return (
-        <div>
-
+        <div className="container ptb">
+            <div className="row">
+                <h2 className="centered mb">I craft handsome sites & stunning apps<br/>that empower your startup.</h2>
+                <div className="col-md-6">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div className="col-md-6">
+                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+                        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                        containing Lorem Ipsum passages, and more recently with
+                        desktop publishing software.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,179 @@
 import React, {Fragment} from 'react';
-import Header from "../components/Header";
+import '../styles/header.css'
+
 
 const HomePage = () => {
     return (
         <Fragment>
-            <Header/>
+            <div id="h">
+                <div className="container centered img_background">
+                    <div className="row">
+                        <div className="col col-md-offset-2 ">
+                            <img src="/img/profil.jpeg" alt="logo" className="logo"/>
+                            <h1>NIEUWJAER Mathias </h1>
+                            <h2>Developpeur WEB</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container ptb">
+                <div className="row">
+                    <h2 className="centered mb col-md-12">Concepteur et developpeur de site internet <br/>pour reppondre
+                        au besoin demander
+                    </h2>
+                    <div className="col-md-6">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                    <div className="col-md-6">
+                        <p>It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of
+                            Letraset sheets containing Lorem Ipsum passages, and more recently with
+                            desktop publishing software.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 col-md-offset-1">
+                        <img src="/img/developpeur-web.jpg" className="img-responsive" alt=""/>
+                    </div>
+                </div>
+            </div>
+            <div className="container mt">
+                <div className="row clients centered">
+                    <p className="mb col-12 ">Languages et framework/library</p>
+                    <div className="col-sm-2 col-sm-offset-1">
+                        <img src="/img/favicon.ico" alt=""/>
+                    </div>
+                    <div className="col-sm-2 ">
+                        <img src="/img/favicon.ico" alt=""/>
+                    </div>
+                    <div className="col-sm-2 ">
+                        <img src="/img/favicon.ico" alt=""/>
+                    </div>
+                    <div className="col-sm-2 ">
+                        <img src="/img/favicon.ico" alt=""/>
+                    </div>
+                    <div className="col-sm-2 ">
+                        <img src="/img/favicon.ico" alt=""/>
+                    </div>
+                </div>
+            </div>
+            <div id="g">
+                <div className="container card-project">
+                    <div className="row-12 work">
+                        <h2>Check some of my latest works.</h2>
+                        <div className="row-12 col-md-offset-2">
+                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text.<br/>It has roots in a
+                                piece of classical Latin literature<br/>from 45 BC, making it over 2000 years old.</p>
+                            <div className="row all-card">
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                                <div className="card col-sm-3 card-project">
+                                    <img src="/img/developpeur-web.jpg" className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and
+                                            make
+                                            up the
+                                            bulk of the card's content.</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="f">
+                <div className="container">
+                    <div className="row centered">
+                        <div className="col-md-8 col-md-offset-2">
+                            <a href="#"><img className="icons" src="img/icons/linkedin.svg" alt=""/></a>
+                            <a href="#"><img className="icons" src="img/icons/gmail.svg" alt=""/></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </Fragment>
     );
 };
