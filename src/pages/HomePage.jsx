ceca23cf2b2dddc89a2f12cf623d98df222b8d1a
Nieuwjaer <mathias.nieuwjaer@gmail.com>
2020-11-15 14:18:56 +0100

    initialisation du projet react

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import Header from "../components/Header";
+
+const HomePage = () => {
+    return (
+        <Fragment>
+            <Header/>
+        </Fragment>
+
+    );
+};
+
+export default HomePage;
